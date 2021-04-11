6b340efcb7d183e4f43bf585e9d17cd87c247e81
Luke Short <lshort2@mail.csuchico.edu>
2021-04-11 15:07:11 -0700

    msg back, messing around

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const utils = require('./utils.js');
 const fs = require('fs');
 const Discord = require('discord.js');
 
-const PABS_CHECK_TIME = 1000 * 60 * 60 * 17 + 43 * 60 * 1000; // 17 hours, 43 min
 const KICK_MESSAGE = 'Yeetus Deletus \nYou are now a fetus';
+const MAX_YEETS_THRESHOLD = 10; // stops affecting move probability at this point
 const token_path = '/Users/Grievous/.hidden_files/discord_yeet_bot.json'
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
 let token = null;
 fs.readFile(token_path, (err, data) => {
@@ -13,25 +13,53 @@ fs.readFile(token_path, (err, data) => {
   initializeBot();
 });
 
-function wordInString(s, word){
-  return new RegExp( '\\b' + word + '\\b', 'i').test(s);
+function movePabs(){
+  const pabs = getPabsUser();
+  // const pabs_channel = getPabsChannel();
+  if (pabs) {
+    const voice_channels = getVoiceChannels();
+  }
 }
 
+
 function initializeBot() {
-  bot.on('ready', () => {
+  client.on('ready', () => {
     console.log('yeety boy here');
+    // console.log('this: ', this);
+    // let data = {};
+    // let textChannel = null;
+    // Discord.Message(this, data, textChannel);
   });
   
-  bot.on('message', msg => {
+  client.on('message', msg => {
     const msg_lowercase = msg.content.toLowerCase();
-    if(utils.wordInString(msg_lowercase, 'yeet')) {
-      if (Math.random() <= utils.getMoveProbability(msg_lowercase)) {
-        utils.movePabs()
+    let yeet_count = utils.getYeetCount(msg_lowercase);
+    if(yeet_count) {
+      if(yeet_count > MAX_YEETS_THRESHOLD) {
+        yeet_count = MAX_YEETS_THRESHOLD;
       }
+      if (Math.random() <= yeet_count/MAX_YEETS_THRESHOLD) {
+        console.log('movemovemove');
+        movePabs()
+      }
+      else {
+        console.log('nonono');
+      }
+    }
+
+    if (msg.author !== client.user) {
+      console.log('say hi');
+      msg.channel.send('hi');
     }
   });
 
-  bot.login(token);
+  client.on('messageReaction', (msgRe, user) => {
+    console.log('msgRe: ', msgRe);
+    console.log('user: ', user);
+  });
+
+
+  client.login(token);
 }
 
 // setInterval(() => {utils.movePabs()}, PABS_CHECK_TIME);
--- utils.js
+++ utils.js
@@ -1,37 +1,43 @@
-function getMoveProbability(message) {
-    const MAX_MOVE_PROBABILITY;
-    let move_probability = 0;
-    message.split(' ').forEach(word => {
-        if(word.includes('yeet') && move_probability < MAX_MOVE_PROBABILITY) {
-            move_probability += 0.1
-            if(move_probability > MAX_MOVE_PROBABILITY) {
-                move_probability = MAX_MOVE_PROBABILITY;
-            }
-        }
-    })
-    return move_probability
-}
+module.exports = {
+  PABS_CHECK_TIME: 1000 * 60 * 60 * 17 + 43 * 60 * 1000, // 17 hours, 43 min
+  MAX_MOVE_PROBABILITY: 0.8,
+  
+  getYeetCount: function(s){
+    let string_to_search = s.toLowerCase();
+    return (string_to_search.match(/yeet/g) || []).length;
+  },
+  
+  getMoveProbability: function(message) {
+      let move_probability = 0;
+      message.split(' ').forEach(word => {
+          if(word.includes('yeet') && move_probability < this.MAX_MOVE_PROBABILITY) {
+              move_probability += 0.1
+              if(move_probability > this.MAX_MOVE_PROBABILITY) {
+                  move_probability = this.MAX_MOVE_PROBABILITY;
+              }
+          }
+      })
+      return move_probability
+  },
+  
+  // recordUserLoginTime: function(username){
+  //   window.setInterval(() => {
+  //     if (inChannel(username)) {
+  //       // update their logged in time += 1 minute
+  //     }
+  //   }, 60 * 1000)
+  // },
+  
+  // moveUserBasedOnLoginTime: function(username){
+  //   window.setInterval(() => {
+  //     if (isLoggedInUser(username)) {
+  //       const logged_in_timer = getLoggedInTime(username)
+  //       if (logged_in_timer > PABS_CHECK_TIME) {
+  //         movePabs()
+  //         resetLoggedInTime(username);
+  //       }
+  //     }
+  //   }, 5 * 60 * 1000)
+  // },
 
-function movePabs(){
-  console.log('test');
-}
-
-function recordUserLoginTime(username){
-  window.setInterval(() => {
-    if (inChannel(username)) {
-      // update their logged in time += 1 minute
-    }
-  }, 60 * 1000)
-}
-
-function moveUserBasedOnLoginTime(username){
-  window.setInterval(() => {
-    if (isLoggedInUser(username)) {
-      const logged_in_timer = getLoggedInTime(username)
-      if (logged_in_timer > PABS_CHECK_TIME) {
-        movePabs()
-        resetLoggedInTime(username);
-      }
-    }
-  }, 5 * 60 * 1000)
-}
+};
