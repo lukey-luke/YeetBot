42e71f2933304e06ee699394f89aa234525860c3
Luke Short <lshort2@mail.csuchico.edu>
2021-04-12 20:12:45 -0700

    kicking functionality, check permissions??

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const MAX_YEETS_THRESHOLD = 10; // stops affecting move probability at this poin
 const token_path = '/Users/Grievous/.hidden_files/discord_yeet_bot.json'
 const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 
+const VOICE_CHANNEL_TYPE = 'voice';
+
 let token = null;
 fs.readFile(token_path, (err, data) => {
   token = JSON.parse(data)["TOKEN"];
@@ -22,6 +24,22 @@ function movePabs(){
 }
 
 
+function moveMessager(msg) {
+  // console.log('msg.guild.name: ', msg.guild.name);
+  const voice_channels = msg.guild.channels.filter(chan => chan.type == VOICE_CHANNEL_TYPE);
+  voice_channels.forEach(v_chan => {
+    console.log(`got ${v_chan.members.size} members in ${v_chan.name}`);
+    v_chan.members.forEach(mem => {
+      // if (mem.user.id == msg.user.id) {
+      if (mem.id == msg.member.id) {
+        console.log('we got him boys: ', mem.user.id);
+        mem.kick('just for fun');
+      }
+    });
+  });
+}
+
+
 function initializeBot() {
   client.on('ready', () => {
     console.log('yeety boy here');
@@ -31,36 +49,40 @@ function initializeBot() {
     // let textChannel = null;
     // Discord.Message(this, data, textChannel);
   });
+
   
   client.on('message', msg => {
+    moveMessager(msg);
+
     const msg_lowercase = msg.content.toLowerCase();
-    let yeet_count = utils.getYeetCount(msg_lowercase);
-    if(yeet_count) {
-      if(yeet_count > MAX_YEETS_THRESHOLD) {
-        yeet_count = MAX_YEETS_THRESHOLD;
+
+    // let bad_word_count = utils.getYeetCount(msg_lowercase);
+    let bad_word_count = utils.getYeetCount(msg_lowercase);
+    if(bad_word_count) {
+      if(bad_word_count > MAX_YEETS_THRESHOLD) {
+        bad_word_count = MAX_YEETS_THRESHOLD;
       }
-      if (Math.random() <= yeet_count/MAX_YEETS_THRESHOLD) {
+      if (Math.random() <= bad_word_count/MAX_YEETS_THRESHOLD) {
         console.log('movemovemove');
         movePabs();
       }
-      else {
-        console.log('nonono');
-      }
     }
 
-    if (msg.author !== client.user) {
-      console.log('say hi');
-      msg.channel.send('hi');
-    }
+    // if (msg.author !== client.user) {
+    //   console.log('say hi');
+    //   msg.channel.send('hi');
+    // }
   });
 
   client.on('messageReactionAdd', (msgRe, user) => {
     // console.log('msgRe: ', msgRe);
     console.log('user: ', user);
     // let user_id = user.id;
+    console.log('msgRe.message:', msgRe.message);
     let guildy = msgRe.message.guild;
-    // console.log('guild? ', guildy);
+    console.log('guild? ', guildy.name);
     let member = guildy.member(user);
+    console.log('member? ', member);
   });
 
 
--- utils.js
+++ utils.js
@@ -1,6 +1,20 @@
+const BAD_WORDS = [
+  'no cap', 'yeet'
+];
+
 module.exports = {
   PABS_CHECK_TIME: 1000 * 60 * 60 * 17 + 43 * 60 * 1000, // 17 hours, 43 min
   MAX_MOVE_PROBABILITY: 0.8,
+  
+  getBadBoyCout: function(s){
+    const string_to_search = s.toLowerCase();
+    let bad_count = 0;
+    BAD_WORDS.forEach(bad_word => {
+      const regex_match = new RegExp('/' + bad_word + '/g');
+      bad_count += (string_to_search.match(regex_match) || []).length;
+    });
+    return bad_count;
+  },
   
   getYeetCount: function(s){
     let string_to_search = s.toLowerCase();
