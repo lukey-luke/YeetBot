e47c39c863f133cb75e7fb5f190ccdce7d4b382c
Luke Short <lshort2@mail.csuchico.edu>
2021-04-19 19:16:15 -0700

    add function to move random voice member to random channel

--- index.js
+++ index.js
@@ -40,15 +40,49 @@ function moveMessager(msg) {
 }
 
 
+function getRandomItem(arr) {
+    const random_index = Math.floor(Math.random() * arr.length);
+    return arr[random_index];
+}
+
+
+function moveRandomVoice(msg) {
+  const voice_channels = msg.guild.channels.cache.filter(chan => chan.type == VOICE_CHANNEL_TYPE);;
+  let all_voice_members = [];
+  voice_channels.forEach(v_chan => {
+    const channel_members = v_chan.members.array();
+    all_voice_members = all_voice_members.concat(channel_members);
+  });
+
+  if (all_voice_members.length) {
+    const randy = getRandomItem(all_voice_members);
+    let current_chan = randy.voice.channel;
+    const new_channels = voice_channels.filter(chan => chan.id !== current_chan.id).array();
+
+    if (new_channels.length) {
+      const new_voice_channel = getRandomItem(new_channels);
+      console.log('move to: ', new_voice_channel.name);
+      randy.voice.setChannel(new_voice_channel, 'all in good fun').then(() => {
+        console.log('user should be in new channel now');
+      });
+    }
+    else {
+      randy.voice.kick('just for fun').then(() => {
+        console.log('should be kicked!');
+      });
+    }
+  }
+}
+
+
 function initializeBot() {
   client.on('ready', () => {
     console.log('yeety boy here');
-    console.log('client.channels: ', client.channels);
   });
 
   
   client.on('message', msg => {
-    moveMessager(msg);
+    moveRandomVoice(msg);
 
     const msg_lowercase = msg.content.toLowerCase();
 
